b91a743f52073187d260318ed5c61481a6dd5ef4
August Karlsson <karlsson.august@gmail.com>
2022-10-09 11:44:53 +0200

    started on ORAMWithReadPathEviction, slightly confused on the pasing of parameter of type UntrustedStorageInterface. Although in job it is specified that the variable is actually of dynamic type ServerStorageForHW

--- pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
+++ pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
@@ -12,15 +12,38 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 	/**
 	 * TODO add necessary variables 
 	 */
-	
+
+
+
+	private UntrustedStorageInterface storage;
+	private RandForORAMInterface rand_gen;
+
+	private ArrayList<Block> stash;
+	private int[] position_map;
+	private int num_buckets;
+	private int num_blocks;
+	private int num_levels;
+	private int num_leaves;
+	private int bucket_size;
+
+
 	public ORAMWithReadPathEviction(UntrustedStorageInterface storage, RandForORAMInterface rand_gen, int bucket_size, int num_blocks){
 		// TODO complete the constructor
+		this.storage = storage;
+		this.rand_gen = rand_gen;
+		this.bucket_size= bucket_size;
+		this.num_blocks = num_blocks;
+
+		Bucket.setMaxSize(bucket_size);
+		Bucket.resetState();
+
 	}
 
 
 	@Override
 	public byte[] access(Operation op, int blockIndex, byte[] newdata) {
 		// TODO Must complete this method for submission
+		
 		return null;
 	}
 
