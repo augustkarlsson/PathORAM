a1ace888c6a03aeddb6848698441cbd8c816c2e5
A-GX <antoine.gansel@tutanota.com>
2022-10-09 17:40:16 +0200

    all variables initialised in OramInterface

--- pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
+++ pathORAM/src/pathOramHw/ORAMWithReadPathEviction.java
@@ -9,22 +9,24 @@ import java.util.ArrayList;
 
 public class ORAMWithReadPathEviction implements ORAMInterface{
 	
-	/**
-	 * TODO add necessary variables 
+	/*
+	 * Already present variables 
 	 */
-
-
-
 	private UntrustedStorageInterface storage;
 	private RandForORAMInterface rand_gen;
 
 	private ArrayList<Block> stash;
 	private int[] position_map;
-	private int num_buckets;
+	private int num_buckets;// Need to be greater or equal to 30.000/4 = 7500 (to feat all write operations)
 	private int num_blocks;
 	private int num_levels;
 	private int num_leaves;
 	private int bucket_size;
+
+	/*
+	 * Student defined variables
+	 */
+	
 
 
 	public ORAMWithReadPathEviction(UntrustedStorageInterface storage, RandForORAMInterface rand_gen, int bucket_size, int num_blocks){
@@ -34,11 +36,35 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 		this.bucket_size= bucket_size;
 		this.num_blocks = num_blocks;
 
+		this.num_buckets = 8000;
+		this.num_levels = this.find_num_levels() 
+		this.num_leaves = this.find_num_leaves()
+
 		Bucket.setMaxSize(bucket_size);
 		Bucket.resetState();
 
+		storage.setCapacity(num_buckets); // We set our capacity to the number of buckets we have
+
 	}
 
+	private int find_num_levels() {
+		int n = 1;
+		while (this.num_buckets % Math.pow(2,n) >= 2) {
+			// we search n such that 2^n <= nb_buckets < 2^(n+1)
+			n++;
+		}
+		return n+1 ;
+	}
+
+	private int find_num_leaves() {
+		int last_leaves = this.num_buckets - (this.num_levels -1) ;
+		int secondLast_leaves = 0;
+		if (this.num_buckets != Math.pow(2,this.num_levels)) {
+			int nodes_secondLast_level = Math.pow(2,this.num_levels-1)-1-Math.pow(2,this.num_levels-2)-1 ;
+			secondLast_leaves = nodes_secondLast_level - Math.ceil(last_leaves/2) ;
+		}
+		return last_leaves + secondLast_leaves ;
+	}
 
 	@Override
 	public byte[] access(Operation op, int blockIndex, byte[] newdata) {
@@ -52,6 +78,7 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 				 *	- [understanding] create a map between bucket tree format (for us) and List format (in untrusted storage).
 				 *		| Also needed for function right below
 				 *	- [execution] need to implement algorithm from slides page 51 to 54
+				 *  - 
 				 */
 				return null;
 			default :
@@ -69,6 +96,13 @@ public class ORAMWithReadPathEviction implements ORAMInterface{
 			 * What need to be done :
 			 *	- create a map between bucket tree format (for us) and List format (in untrusted storage)
 			 *	- output the result of this map
+			 * ex : 123456789
+			 			1
+					  /   \
+					 2     3
+					/ \   / \
+				   4   5 6   7
+				   8   9
 			 */
 		return 0;
 	}
